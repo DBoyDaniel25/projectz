a6d31508fdb09fb28b8bc4178573dea774530d0b
Daniel Prince <programmer96@hotmail.com>
2016-01-26 20:05:46 -0400

    Reformatted code

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
 // plugins
-var gulp   = require("gulp"),
-    render = require("gulp-nunjucks-render"),
+var gulp        = require("gulp"),
+    render      = require("gulp-nunjucks-render"),
     ext_replace = require('gulp-ext-replace'),
     // locations
-    src    = "./cms/",
-    cms    = {
+    src         = "./cms/",
+    cms         = {
         templates: [
-           "./templates/cms/nunjucks/",
-           "./templates/cms/base/",
-           "./templates/cms/forms/",
-           "./templates/cms/partials/",
-           "./templates/cms/php/"
+            "./templates/cms/nunjucks/",
+            "./templates/cms/base/",
+            "./templates/cms/forms/",
+            "./templates/cms/partials/",
+            "./templates/cms/php/"
         ],
-        pages    :"./templates/cms/pages/*.nunjucks"
+        pages    : "./templates/cms/pages/*.nunjucks"
     };
 
 
 gulp.task("render", function () {
-    render.nunjucks.configure(cms.templates,{ watch : false});
+    render.nunjucks.configure(cms.templates, {watch: false});
 
     return gulp.src(cms.pages)
         .pipe(render())
