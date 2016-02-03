4c14cb0c845787fea109f13731c0e7769bd4b6db
Daniel Prince <programmer96@hotmail.com>
2016-02-03 17:58:19 -0400

    removed avatar

--- cms/memory_edit.php
+++ cms/memory_edit.php
@@ -156,16 +156,6 @@
                         <li class="hidden-xs">
                             <a href="#" class="right-bar-toggle waves-effect waves-light"><i class="md md-chat"></i></a>
                         </li>
-                        <li class="dropdown">
-                            <a href="" class="dropdown-toggle profile" data-toggle="dropdown" aria-expanded="true"><img
-                                        src="images/avatar-1.jpg" alt="user-img" class="img-circle"> </a>
-                            <ul class="dropdown-menu">
-                                <li><a href="javascript:void(0)"><i class="md md-face-unlock"></i> Profile</a></li>
-                                <li><a href="javascript:void(0)"><i class="md md-settings"></i> Settings</a></li>
-                                <li><a href="javascript:void(0)"><i class="md md-lock"></i> Lock screen</a></li>
-                                <li><a href="javascript:void(0)"><i class="md md-settings-power"></i> Logout</a></li>
-                            </ul>
-                        </li>
                     </ul>
                 </div>
                 <!--/.nav-collapse -->
