9f67ecc5588238306e64c78f27a5334b6e65bc0b
Daniel Prince <programmer96@hotmail.com>
2016-02-03 17:13:45 -0400

    minified vendor code

--- cms/gulpfile.js
+++ cms/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp         = require("gulp"),
     render       = require("gulp-nunjucks-render"),
     ext_replace  = require("gulp-ext-replace"),
+    minify_js = require("gulp-uglify"),
     baseTemplate = "./template/",
     template     = {
         source: [
@@ -9,6 +10,10 @@ var gulp         = require("gulp"),
             baseTemplate + "main/"
         ],
         pages : baseTemplate + "pages/**/*.twig"
+    },
+    js = {
+        myscripts : "./js/scripts/**/*.*",
+        vendor : "./js/*.js"
     };
 
 gulp.task("render", function () {
@@ -17,4 +22,11 @@ gulp.task("render", function () {
         .pipe(render())
         .pipe(ext_replace(".php"))
         .pipe(gulp.dest("./"));
+});
+
+
+gulp.task("minify-js", function(){
+    return gulp.src(js.vendor)
+        .pipe(minify_js())
+        .pipe(gulp.dest('./js/'));
 });
