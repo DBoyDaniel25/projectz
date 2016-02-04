619b72e9928be95224510b219e9d34528776db50
Daniel Prince <programmer96@hotmail.com>
2016-02-04 16:22:35 -0400

    reassurance table CRUD file

---
+++ cms/Backend/manage/reassure.php
@@ -0,0 +1,45 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: evolutionarycoder
+     * Date: 2/4/16
+     * Time: 4:19 PM
+     */
+    include "../../vendor/autoload.php";
+    if (isset($_POST["create"])) {
+        $reassure = $_POST[\Backend\Database\Tables\Reassurance::REASSURE];
+
+        $obj = new \Backend\Database\Schemas\Reassure(null, $reassure);
+
+        $table = new \Backend\Database\Tables\Reassurance();
+        if ($table->create($obj)) {
+            echo "true";
+        } else {
+            echo "false";
+        }
+    }
+
+    if (isset($_POST["update"])) {
+
+        $reassure = $_POST[\Backend\Database\Tables\Reassurance::REASSURE];
+        $id       = $_POST[\Backend\Database\Tables\Reassurance::ROW_ID];
+
+        $obj = new \Backend\Database\Schemas\Reassure($id, $reassure);
+
+        $table = new \Backend\Database\Tables\Reassurance();
+        if ($table->update($obj)) {
+            echo "true";
+        } else {
+            echo "false";
+        }
+    }
+
+    if (isset($_POST["delete"])) {
+        $id    = $_POST[\Backend\Database\Tables\Reassurance::ROW_ID];
+        $table = new \Backend\Database\Tables\Reassurance();
+        if ($table->delete($id)) {
+            echo "true";
+        } else {
+            echo "false";
+        }
+    }
