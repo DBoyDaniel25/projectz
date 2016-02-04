7519bb5186269baa9d4a1d9b8f586709e7d9db79
Daniel Prince <programmer96@hotmail.com>
2016-02-04 13:04:31 -0400

    loads all ilove poems

---
+++ cms/template/php/load_iloves.php
@@ -0,0 +1,31 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: evolutionarycoder
+     * Date: 2/4/16
+     * Time: 1:01 PM
+     */
+
+    use Backend\Database\Tables\ILove;
+    use Backend\Helpers\TableBuilder;
+
+    include "vendor/autoload.php";
+
+    $table   = new ILove();
+    $data    = $table->readAll();
+    $builder = new TableBuilder();
+    if ($data !== false) {
+        for ($i = 0; $i < count($data); $i++) {
+            $decoded = clone $data[$i];
+            $table->htmlDecode($decoded);
+            $current = $data[$i];
+            $builder->buildCell($current->getLove())->
+                      buildCell($current->getSynced()
+            );
+            $builder->addActionAttrs("ilove", $decoded->getLove())->addActionAttrs("id", $decoded->getId());
+            $builder->addRowAttr("id", $current->getId());
+            echo $builder->buildRow();
+        }
+    }
+
+?>
