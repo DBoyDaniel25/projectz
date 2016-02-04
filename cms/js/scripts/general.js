e6ae6c532d0862b4d5753c5d8ff0c0be639ce823
Daniel Prince <programmer96@hotmail.com>
2016-02-04 11:55:38 -0400

    hooked up close button

--- cms/js/scripts/general.js
+++ cms/js/scripts/general.js
@@ -7,24 +7,32 @@ $(".row.form").fadeOut();
 // set up the edit, delete buttons
 var editBtn      = document.querySelectorAll(".edit-row"),
     deleteBtn    = document.querySelectorAll(".remove-row"),
+    closeBtn        = document.querySelector("#close"),
+    closeMethod  = function () {
+        // show edit form
+        $(".row.form").fadeOut();
+    },
     editMethod   = function (e) {
         e.preventDefault();
 
-        if(manage.processFile.indexOf("poem") !== -1){
+        if (manage.processFile.indexOf("poem") !== -1) {
             poem.getEditButtonData(this).populateForm(true);
+        }
+
+        if (manage.processFile.indexOf("ilove") !== -1) {
+            ilove.getEditButtonData(this).populateForm(true);
         }
 
         // show edit form
         $(".row.form").fadeIn();
-
     },
     deleteMethod = function (e) {
         e.preventDefault();
         var rowId = this.getAttribute("data-id");
         var data  = {
-                "delete": "delete",
-                id      : rowId
-            };
+            "delete": "delete",
+            id      : rowId
+        };
         manage.performDelete(data);
     };
 
@@ -32,5 +40,8 @@ for (var i = 0; i < editBtn.length; i++) {
     try {
         editBtn[i].addEventListener("click", editMethod, false);
         deleteBtn[i].addEventListener("click", deleteMethod, false);
-    }catch (e){}
+    } catch (e) {
+    }
 }
+
+closeBtn.addEventListener("click", closeMethod, false);
